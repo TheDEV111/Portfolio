dbda9f06bec88bd9f76a650e37aaf1630e22190a
henryno111 <h.agukwe@yahoo.com>
2024-09-17 13:05:51 +0100

    creating Icon navlinks

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,25 +1,40 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import imageProfile from '../Assets/imageProfile.JPG';
+import { GrProjects } from "react-icons/gr";
+import { FaPhoneFlip } from "react-icons/fa6";
+import { IconBase } from "react-icons";
+import { FaHome } from "react-icons/fa";
+
 
 
 const Navbar = () => {
+
+    const navLinks = [
+      {icon: <GrProjects />, path: "/projects"},
+      {icon: <FaHome />, path: "/projects"},
+      {icon: <IconBase />, path: "/projects"},
+      {icon: <FaPhoneFlip />, path: "/projects"},
+    ]
   return (
     <div>
       <div>
-          <img
-            src={imageProfile} 
-            alt="imagePortfolio"
-            className="h-28 w-24 rounded-full p-1 object-cover"
-            />
-          <p className="font-bold">Henry Agukwe</p>
+        <img
+          src={imageProfile} 
+          alt="imagePortfolio"
+          className="h-[3%] w-[3%] rounded-full p-1 object-contain"
+        />
+        <p className="font-bold">Henry Agukwe</p>
       </div>
-      <div>
-        <NavLink to="/" className="p-4">Home</NavLink>
-        <NavLink to="/about" className="p-4">About</NavLink>
-        <NavLink to="/projects" className="p-4">Projects</NavLink>
-        <NavLink to="/contact" className="p-4">Contact</NavLink>
-      </div>
+      <ul className="flex flex-row gap-20" >
+          {navLinks.map((link, index) => (
+              <li key={index}>
+                  <NavLink to={link.path} activeClassName="text-blue-500" className="text-green-600">
+                      {link.icon}
+                  </NavLink>
+              </li>
+          ))}
+      </ul>
     </div>
   )
 }
